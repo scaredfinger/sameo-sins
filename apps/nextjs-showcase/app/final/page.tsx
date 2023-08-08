29fa86b4da69fb66ce301d0fa13d35c5a4bae116
Henry Villa <henry.villa@sembo.se>
2023-08-08 14:45:15 +0200

    add styles to cards and button and headlines

--- apps/nextjs-showcase/app/final/page.tsx
+++ apps/nextjs-showcase/app/final/page.tsx
@@ -2,14 +2,14 @@
 
 import { useEffect } from 'react';
 
+import { loadCategories, loadCardsByCategory, Trip } from '../with-actual-data/api'
 import { useCardsByCategoriesState } from '../../components/use-cards-by-categories-state';
 import { CardList } from 'apps/nextjs-showcase/components/card-list';
 
 import styles from './page.module.scss';
-import { Trip, TripCollection, loadCardsByCategory, loadCategories } from './api';
 
 export default function Index() {
-  const viewState = useCardsByCategoriesState<Trip, TripCollection>({
+  const viewState = useCardsByCategoriesState<Trip, string>({
     loadCategories,
     loadCardsByCategory,
     numberOfCategoriesToPreload: 2,
@@ -31,12 +31,12 @@ export default function Index() {
 
 function renderCards(card: Trip) {
   return <div className={`${styles.card} ${styles.done}`}>
-    <h2>{card.headline}</h2>
+    <h3>{card.headline}</h3>
     <p>{card.salesPitch}</p>
     <div className={styles.cardImageList}>
-      {card.images.map((image, i) => (
-        <img key={i} src={image} className={styles.cardImage} />
-      ))}
+          {card.images.map((image, i) => (
+            <img key={i} src={image} className={styles.cardImage} />
+          ))}
     </div>
   </div>
 }
