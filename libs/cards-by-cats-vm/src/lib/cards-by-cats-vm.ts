b02c5a30356357a86a3522c5b03d748e11249610
Erich Ledesma <scaredfinger@gmail.com>
2023-08-08 12:21:42 +0200

    Styled

--- libs/cards-by-cats-vm/src/lib/cards-by-cats-vm.ts
+++ libs/cards-by-cats-vm/src/lib/cards-by-cats-vm.ts
@@ -1,68 +1,68 @@
-import { AsyncData, Future, Result } from '@swan-io/boxed';
+import { AsyncData, Future, Result } from '@swan-io/boxed'
 
-export type AsyncFunc<T = unknown> = () => Future<Result<T, Error>>;
+export type AsyncFunc<T = unknown> = () => Future<Result<T, Error>>
 
-export type WithCardinality<Category> = [Category, number];
+export type WithCardinality<Category> = [Category, number]
 
-export type SubmitChangesFunc<T> = (value: T) => T;
+export type SubmitChangesFunc<T> = (value: T) => T
 
 export interface CategoryWithCards<Category, Card> {
-  category: Category;
-  cards: Card[];
+  category: Category
+  cards: Card[]
 }
 
-export type AsyncResult<T> = AsyncData<Result<T, Error>>;
+export type AsyncResult<T> = AsyncData<Result<T, Error>>
 
 export function ok<T>(value: T): AsyncResult<T> {
-  return AsyncData.Done(Result.Ok(value));
+  return AsyncData.Done(Result.Ok(value))
 }
 
 export function fail<T>(error: Error): AsyncResult<T> {
-  return AsyncData.Done(Result.Error(error));
+  return AsyncData.Done(Result.Error(error))
 }
 
 export interface State<Card = never, Category = never> {
-  readonly categories: AsyncData<Result<WithCardinality<Category>[], Error>>;
+  readonly categories: AsyncData<Result<WithCardinality<Category>[], Error>>
   submitCategoryChanges(
     applyChanges: SubmitChangesFunc<
       AsyncData<Result<WithCardinality<Category>[], Error>>
     >
-  ): void;
+  ): void
 
-  readonly cardsByCategory: AsyncResult<Map<Category, AsyncResult<Card>[]>>;
+  readonly cardsByCategory: AsyncResult<Map<Category, AsyncResult<Card>[]>>
   submitCardsByCategoryChanges(
     applyChanges: SubmitChangesFunc<
       AsyncResult<Map<Category, AsyncResult<Card>[]>>
     >
-  ): void;
+  ): void
 }
 
 interface CardsByCategoriesWithProgressiveLoadingOptions {
-  numberOfCategoriesToPreload?: number;
-  numberOfCardsToPreload?: number;
-  numberOfLoadMoreCategories?: number;
-  numberOfLoadMoreCards?: number;
+  numberOfCategoriesToPreload?: number
+  numberOfCardsToPreload?: number
+  numberOfLoadMoreCategories?: number
+  numberOfLoadMoreCards?: number
 }
 
 export class CardsByCategoriesWithProgressiveLoading<
   Card = never,
   Category = string
 > {
-  private numberOfCategoriesToPreload: number;
-  private numberOfCardsToPreload: number;
-  private numberOfLoadMoreCategories: number;
-  private numberOfLoadMoreCards: number;
+  private numberOfCategoriesToPreload: number
+  private numberOfCardsToPreload: number
+  private numberOfLoadMoreCategories: number
+  private numberOfLoadMoreCards: number
 
   public get categories(): AsyncData<
     Result<WithCardinality<Category>[], Error>
   > {
-    return this.state.categories;
+    return this.state.categories
   }
 
   public get cardsByCategory(): AsyncResult<
     Map<Category, AsyncResult<Card>[]>
   > {
-    return this.state.cardsByCategory;
+    return this.state.cardsByCategory
   }
 
   constructor(
@@ -80,40 +80,40 @@ export class CardsByCategoriesWithProgressiveLoading<
       numberOfLoadMoreCards = 2,
     }: CardsByCategoriesWithProgressiveLoadingOptions = {}
   ) {
-    this.numberOfCategoriesToPreload = numberOfCategoriesToPreload;
-    this.numberOfCardsToPreload = numberOfCardsToPreload;
-    this.numberOfLoadMoreCategories = numberOfLoadMoreCategories;
-    this.numberOfLoadMoreCards = numberOfLoadMoreCards;
+    this.numberOfCategoriesToPreload = numberOfCategoriesToPreload
+    this.numberOfCardsToPreload = numberOfCardsToPreload
+    this.numberOfLoadMoreCategories = numberOfLoadMoreCategories
+    this.numberOfLoadMoreCards = numberOfLoadMoreCards
   }
 
   public preload() {
-    this.state.submitCategoryChanges(() => AsyncData.Loading());
-    this.state.submitCardsByCategoryChanges(() => AsyncData.Loading());
+    this.state.submitCategoryChanges(() => AsyncData.Loading())
+    this.state.submitCardsByCategoryChanges(() => AsyncData.Loading())
 
     this.loadCategories()
       .mapOk((categories) => {
-        this.state.submitCategoryChanges(() => ok(categories));
+        this.state.submitCategoryChanges(() => ok(categories))
 
-        const cardsByCategory = new Map<Category, AsyncResult<Card>[]>();
+        const cardsByCategory = new Map<Category, AsyncResult<Card>[]>()
         categories.forEach(([category, cardinality]) => {
           cardsByCategory.set(
             category,
             Array(cardinality).fill(AsyncData.NotAsked())
-          );
-        });
-        this.state.submitCardsByCategoryChanges(() => ok(cardsByCategory));
+          )
+        })
+        this.state.submitCardsByCategoryChanges(() => ok(cardsByCategory))
 
         this.loadCardsForCategoryRange({
           from: 0,
           to: this.numberOfCategoriesToPreload,
           categories,
           cardsByCategory,
-        });
+        })
       })
       .mapError((error) => {
-        this.state.submitCategoryChanges(() => fail(error));
-        this.state.submitCardsByCategoryChanges(() => fail(error));
-      });
+        this.state.submitCategoryChanges(() => fail(error))
+        this.state.submitCardsByCategoryChanges(() => fail(error))
+      })
   }
 
   private loadCardsForCategoryRange({
@@ -122,14 +122,14 @@ export class CardsByCategoriesWithProgressiveLoading<
     categories,
     cardsByCategory,
   }: {
-    from: number;
-    to: number;
-    categories: WithCardinality<Category>[];
-    cardsByCategory: Map<Category, AsyncResult<Card>[]>;
+    from: number
+    to: number
+    categories: WithCardinality<Category>[]
+    cardsByCategory: Map<Category, AsyncResult<Card>[]>
   }) {
-    const categoriesToLoad = categories.slice(from, to);
+    const categoriesToLoad = categories.slice(from, to)
 
-    this.loadCardsForSpecifiedCategories({ categoriesToLoad, cardsByCategory });
+    this.loadCardsForSpecifiedCategories({ categoriesToLoad, cardsByCategory })
   }
 
   private loadCardsForSpecifiedCategories({
@@ -138,12 +138,12 @@ export class CardsByCategoriesWithProgressiveLoading<
     limit = this.numberOfCardsToPreload,
     cardsByCategory,
   }: {
-    categoriesToLoad: WithCardinality<Category>[];
-    offset?: number;
-    limit?: number;
-    cardsByCategory: Map<Category, AsyncResult<Card>[]>;
+    categoriesToLoad: WithCardinality<Category>[]
+    offset?: number
+    limit?: number
+    cardsByCategory: Map<Category, AsyncResult<Card>[]>
   }) {
-    const loadingCardsLength = limit;
+    const loadingCardsLength = limit
 
     this.state.submitCardsByCategoryChanges((current) =>
       mergeValuesForLoading(
@@ -152,25 +152,20 @@ export class CardsByCategoriesWithProgressiveLoading<
         offset,
         loadingCardsLength
       )
-    );
+    )
 
     this.loadCardsByCategory(
       categoriesToLoad.map(([category]) => category),
       offset,
       limit
     ).mapOk((loadedCardsByCategory) => {
-      const from = offset;
-      const to = from + limit;
+      const from = offset
+      const to = from + limit
 
       this.state.submitCardsByCategoryChanges((current) => {
-        return mergeValuesWhenLoadDone(
-          current,
-          loadedCardsByCategory,
-          from,
-          to
-        );
-      });
-    });
+        return mergeValuesWhenLoadDone(current, loadedCardsByCategory, from, to)
+      })
+    })
   }
 
   public loadMoreCategories() {
@@ -178,13 +173,13 @@ export class CardsByCategoriesWithProgressiveLoading<
       this.cardsByCategory.mapOk((cardsByCategory) => {
         const countOfCategoriesWithAskedOrLoadedCards = Array.from(
           cardsByCategory.values()
-        ).filter((cards) => cards.some((card) => !card.isNotAsked())).length;
+        ).filter((cards) => cards.some((card) => !card.isNotAsked())).length
 
-        const from = countOfCategoriesWithAskedOrLoadedCards;
-        const to = from + this.numberOfLoadMoreCategories;
+        const from = countOfCategoriesWithAskedOrLoadedCards
+        const to = from + this.numberOfLoadMoreCategories
 
         if (to > categories.length) {
-          return;
+          return
         }
 
         this.loadCardsForCategoryRange({
@@ -192,26 +187,26 @@ export class CardsByCategoriesWithProgressiveLoading<
           to,
           categories,
           cardsByCategory,
-        });
-      });
-    });
+        })
+      })
+    })
   }
 
   public loadMoreCards(category: Category) {
     this.categories.mapOk((categories) => {
       this.cardsByCategory.mapOk((cardsByCategory) => {
-        const cards = cardsByCategory.get(category) ?? [];
+        const cards = cardsByCategory.get(category) ?? []
 
-        const offset = cards.findIndex((c) => c.isNotAsked());
+        const offset = cards.findIndex((c) => c.isNotAsked())
 
         if (offset === -1) {
-          return;
+          return
         }
 
-        let limit = this.numberOfLoadMoreCards;
+        let limit = this.numberOfLoadMoreCards
 
         if (offset + limit >= cards.length) {
-          limit = cards.length - offset;
+          limit = cards.length - offset
         }
 
         this.loadCardsForSpecifiedCategories({
@@ -219,9 +214,9 @@ export class CardsByCategoriesWithProgressiveLoading<
           offset,
           limit,
           cardsByCategory,
-        });
-      });
-    });
+        })
+      })
+    })
   }
 }
 
@@ -234,26 +229,26 @@ function mergeValuesForLoading<Category, Card>(
   return current.mapOk((cardsByCategories) => {
     const cardsByCategoriesWithLoading = new Map<Category, AsyncResult<Card>[]>(
       cardsByCategories
-    );
+    )
 
     categoriesToLoad.forEach(([category, cardinality]) => {
-      const categoryCards = cardsByCategoriesWithLoading.get(category) ?? [];
+      const categoryCards = cardsByCategoriesWithLoading.get(category) ?? []
 
-      const cardsBefore = categoryCards.slice(0, offset);
-      const loadingCards = Array(loadingCardsLength).fill(AsyncData.Loading());
+      const cardsBefore = categoryCards.slice(0, offset)
+      const loadingCards = Array(loadingCardsLength).fill(AsyncData.Loading())
       const nonAskedCards = Array(
         cardinality - loadingCardsLength - offset
-      ).fill(AsyncData.NotAsked());
+      ).fill(AsyncData.NotAsked())
 
       cardsByCategoriesWithLoading.set(category, [
         ...cardsBefore,
         ...loadingCards,
         ...nonAskedCards,
-      ]);
-    });
+      ])
+    })
 
-    return cardsByCategoriesWithLoading;
-  });
+    return cardsByCategoriesWithLoading
+  })
 }
 
 function mergeValuesWhenLoadDone<Category, Card>(
@@ -263,18 +258,18 @@ function mergeValuesWhenLoadDone<Category, Card>(
   to: number
 ): AsyncResult<Map<Category, AsyncResult<Card>[]>> {
   return current.mapOk((cardsByCategories) => {
-    const newValue = new Map<Category, AsyncResult<Card>[]>(cardsByCategories);
+    const newValue = new Map<Category, AsyncResult<Card>[]>(cardsByCategories)
     loadedCardsByCategory.forEach((cards, category) => {
-      const existingCards = cardsByCategories.get(category) ?? [];
+      const existingCards = cardsByCategories.get(category) ?? []
 
-      const cardsBefore = existingCards.slice(0, from);
-      const cardsAfter = existingCards.slice(to);
+      const cardsBefore = existingCards.slice(0, from)
+      const cardsAfter = existingCards.slice(to)
 
-      const newCards = [...cardsBefore, ...cards, ...cardsAfter];
+      const newCards = [...cardsBefore, ...cards, ...cardsAfter]
 
-      newValue.set(category, newCards);
-    });
+      newValue.set(category, newCards)
+    })
 
-    return newValue;
-  });
+    return newValue
+  })
 }
